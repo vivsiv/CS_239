4406afc6388266b019bcae4b69c5408b2124914f
Vivek <vivek.a.sivakumar@gmail.com>
2016-02-04 21:21:45 -0800

    add generate call stack

---
+++ generate_call_stack.py
@@ -0,0 +1,15 @@
+num_tabs = 0
+for line in open('out.txt', 'r'):
+	if ('[Call end]' in line) or ('[Call begin]' in line):
+		if ('[Call end]' in line):
+			num_tabs -= 1
+
+		line_string = ""
+		for i in range(0,num_tabs):
+			line_string += "\t"
+
+		line_string += ": ".join(line.split("$$"))
+		print line_string.rstrip()
+
+		if ('[Call begin]' in line): 
+			num_tabs += 1
