d3d2fc76c660f14ccd938a1b14757ab3d001e1e1
Chien-Ning Chen <chienningchen@gmail.com>
2016-02-07 16:33:06 -0800

    File clean-up.

--- CCT_Analysis_Tool/batch_parse.py
+++ CCT_Analysis_Tool/batch_parse.py
@@ -5,6 +5,9 @@ directory_in = sys.argv[1]
 directory_out = sys.argv[2]
 for log_file in os.listdir(directory_in):
 	if log_file.endswith(".txt"):
+		print ("Processing file " + log_file)
+
 		script_call = "python parse_tree.py " + directory_in + "/" + log_file + " "
-		script_call += (directory_out + "/" + log_file.split(".")[0] + ".csv")
+		script_call += (directory_out + "/" + log_file + ".csv")
+
 		os.system(script_call)
