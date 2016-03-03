eb2df1a830ae1664921ee0cf1a2f765c1a75e8f3
Vivek <vivek.a.sivakumar@gmail.com>
2016-03-03 12:45:51 -0800

    clean up of machine learning

--- ml/basic.py
+++ ml/basic.py
@@ -8,15 +8,16 @@ import sklearn as sk
 import sklearn.cluster
 import sklearn.ensemble
 
-def convert_pass_fail(pf):
-	if pf == "pass":
-		return 1
-	else:
-		return 0
+# def convert_pass_fail(pf):
+# 	if pf == "pass":
+# 		return 1
+# 	else:
+# 		return 0
 
 known_test_cases = sys.argv[1]
 test_cases = pandas.read_csv(known_test_cases)
 # test_cases["pass/fail"].apply(convert_pass_fail)
+print test_cases
 
 features = ["longest_1","longest_2","longest_3","longest_4","longest_5"]
 model = sklearn.ensemble.RandomForestClassifier(n_estimators=5)
@@ -28,6 +29,7 @@ to_predict = sys.argv[2]
 tests_to_predict = pandas.read_csv(to_predict)
 # tests_to_predict["pass/fail"].apply(convert_pass_fail)
 predictions = model.predict(tests_to_predict[features])
+print predictions
 
 result = pandas.DataFrame({
 	"test_name": tests_to_predict["test_name"],
