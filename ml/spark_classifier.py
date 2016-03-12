2d9b412b0a9472dc6a2588e0bb6d2fbc64730734
Vivek <vivek.a.sivakumar@gmail.com>
2016-03-11 16:00:46 -0800

    update classifier to take a directory of test cases and make csv of results

--- ml/spark_classifier.py
+++ ml/spark_classifier.py
@@ -11,18 +11,6 @@ import sys
 import csv
 import math
 
-test_csv = sys.argv[1]
-pct_train = float(sys.argv[2])
-classifier = sys.argv[3]
-
-all_data = pandas.read_csv(test_csv)
-end_train = int(math.floor(pct_train * len(all_data)))
-train_data = all_data[:end_train]
-predict_data = all_data[end_train:]
-headers = list(all_data.columns.values)
-features = headers[1:-1]
-print "Features: features"
-
 def forest_classifier(features,train):
 	model = sklearn.ensemble.RandomForestClassifier(n_estimators=5)
 	scores = sklearn.cross_validation.cross_val_score(model, train[features], train["pass/fail"], cv=2)
@@ -95,11 +83,6 @@ def grid_search_spark(sc,features,train,predict):
 		grid_model.fit(train[features], train["pass/fail"])
 		grid_searches[model_name] = grid_model
 
-    	# print("Our best score here:", grid_model.best_score_)
-    	# print("Our best params here:",grid_model.best_params_)
-    	# best_scores.append(grid_model.best_score_)
-    	# best_models.append(grid_model)
-
 	best_model = None
 	best_score = 0
 	for name,gs in grid_searches.iteritems():
@@ -122,23 +105,87 @@ def predict(model,predict_data,features):
 	print result
 
 	correct = [i for i, j in zip(predict_data["pass/fail"], predictions) if i == j]
-	print ("Percent Correct %f" % (float(len(correct))/len(predictions)))
+	pct_correct = (float(len(correct))/len(predictions))
+	print ("Percent Correct %f" % pct_correct)
+	return pct_correct
 
-model = None
-if classifier == "tree":
-	model = forest_classifier(features,train_data)
-elif classifier == "linear":
-	model = logistic_classifier(features,train_data)
-elif classifier == "bayes":
-	model = bayes_classifier(features,train_data)
-elif classifier == "grid":
-	model = grid_search(features,train_data,predict_data)
-else:
-	import spark_sklearn
-	from spark_sklearn import GridSearchCV
-	from pyspark import SparkContext 
-	sc = SparkContext("local","Metric Classifier")
+def classify(test_csv,pct_train,classifier):
+	all_data = pandas.read_csv(test_csv)
+	end_train = int(math.floor(pct_train * len(all_data)))
+	train_data = all_data[:end_train]
+	predict_data = all_data[end_train:]
+	headers = list(all_data.columns.values)
+	features = headers[1:-1]
 
-	model = grid_search_spark(sc,features,train_data,predict_data)
+	model = None
+	if classifier == "tree":
+		model = forest_classifier(features,train_data)
+	elif classifier == "linear":
+		model = logistic_classifier(features,train_data)
+	elif classifier == "bayes":
+		model = bayes_classifier(features,train_data)
+	elif classifier == "grid":
+		model = grid_search(features,train_data,predict_data)
+	else:
+		import spark_sklearn
+		from spark_sklearn import GridSearchCV
+		from pyspark import SparkContext 
+		sc = SparkContext("local","Metric Classifier")
 
-predict(model,predict_data,features)
+		model = grid_search_spark(sc,features,train_data,predict_data)
+
+	percent_correct = predict(model,predict_data,features)
+	return [test_csv.split(".")[0],percent_correct]
+
+def main():
+	input_dir,pct_train,classifier = None,None,None
+	try:
+		input_dir = sys.argv[1]
+		pct_train = float(sys.argv[2])
+		classifier = sys.argv[3]
+	except:
+		print "ERROR, Usage: python bulk_classifier.py <input_dir> <pct_train> <classifier>"
+		print "\t<input_dir> Directory containing test case csv's"
+		print "\t<pct_train> Percentage of csv to use as training data (0,1)"
+		print "\t<classifier> Type of classifier to use: tree, linear, bayes, grid, spark"
+		exit(1)
+
+	with open("classifier_results.csv", "w") as csvOutput:
+		writer = csv.writer(csvOutput, delimiter=',', quotechar='"')
+		writer.writerow(["feature_name","percent_correct"])
+		for test_file in os.listdir(input_dir):
+			if test_file.endswith(".csv"):
+				test_csv = "{0}/{1}".format(input_dir,test_file)
+				result_row = classify(test_csv,pct_train,classifier)
+				print result_row
+				writer.writerow(result_row)
+
+	# all_data = pandas.read_csv(test_csv)
+	# end_train = int(math.floor(pct_train * len(all_data)))
+	# train_data = all_data[:end_train]
+	# predict_data = all_data[end_train:]
+	# headers = list(all_data.columns.values)
+	# features = headers[1:-1]
+
+	# model = None
+	# if classifier == "tree":
+	# 	model = forest_classifier(features,train_data)
+	# elif classifier == "linear":
+	# 	model = logistic_classifier(features,train_data)
+	# elif classifier == "bayes":
+	# 	model = bayes_classifier(features,train_data)
+	# elif classifier == "grid":
+	# 	model = grid_search(features,train_data,predict_data)
+	# else:
+	# 	import spark_sklearn
+	# 	from spark_sklearn import GridSearchCV
+	# 	from pyspark import SparkContext 
+	# 	sc = SparkContext("local","Metric Classifier")
+
+	# 	model = grid_search_spark(sc,features,train_data,predict_data)
+
+	# percent_correct = predict(model,predict_data,features)
+	# return [test_csv.split(".")[0],percent_correct]
+
+if __name__ == "__main__":
+    main()
